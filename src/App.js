a29a6d0276249e5a30396a20c0907b2d89b2f671
lpereira <lucasbarbosaj.pereira@gmail.com>
2021-11-17 22:42:21 -0300

    Added header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
-import MovieRow from './components/MovieRow/MovieRow';
+import MovieRow from './components/MovieRow';
 import './App.css';
-import FeaturedMovie from './components/FeaturedMovie/FeaturedMovie';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
 export default function App() {
 
   const [movieList, setMoviewList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -27,9 +29,27 @@ export default function App() {
     loadAll();
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true)
+      } else {
+        setBlackHeader(false)
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return ()=> {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, []);
+
   return (
     
     <div className="page">
+      <Header black={blackHeader}/>
+      
       {featuredData && 
         <FeaturedMovie item={featuredData}/>
       }
